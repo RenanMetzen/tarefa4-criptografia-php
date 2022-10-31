020742a1fb06753a648c984f2ca48aa55da5df2d
renanmetzenif <renanmetzenif@gmail.com>
2022-10-31 17:26:52 -0300

    geração de chaves assimétricas completa

--- api/controller.php
+++ api/controller.php
@@ -2,20 +2,24 @@
 include('Crypt/RSA.php');
 
 if($_POST['tipo'] == 'gerarChaves'){
-  array_map('unlink', glob("*.txt"));
   $arrayChaves = array();
   $rsa = new Crypt_RSA();
   extract($rsa->createKey());
+
   $arrayChaves = [$privatekey, $publickey];
-  $chavePrivada = fopen((__DIR__) . '/' . $_POST['nome'] . ".pr" . ".txt", "w") or die("Unable to open file!");
+
+  $chavePrivada = fopen((__DIR__) . '/chave.pr.txt', "w") or die("Unable to open file!");
   $txt = $privatekey;
   fwrite($chavePrivada, $txt);
   fclose($chavePrivada);
-  $chavePublica = fopen((__DIR__) . '/' . $_POST['nome'] . ".pu" . ".txt", "w") or die("Unable to open file!");
+
+  $chavePublica = fopen((__DIR__) . '/chave.pu.txt', "w") or die("Unable to open file!");
   $txt = $publickey;
   fwrite($chavePublica, $txt);
   fclose($chavePublica);
+
+
 }
-echo json_encode(true);
+echo json_encode($_POST['nome']);
 ?>
 

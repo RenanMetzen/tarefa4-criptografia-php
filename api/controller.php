2bfefe64d451783148aa8d6b2eca60683da17861
Renan Metzen <renanmetzen@caiweb.com.br>
2022-11-19 17:19:26 -0300

    enc simétrica ok

--- api/controller.php
+++ api/controller.php
@@ -9,16 +9,39 @@ if(isset($_POST['chaves'])){
   $arrayChaves = [$privatekey, $publickey];
   echo json_encode($arrayChaves);
 }elseif(isset($_FILES['texto'])){
+  $arrayEncrypt = array();
+
   ini_set( 'default_charset', 'utf-8');
   $formatosPermitidos = "txt";
   $extensao = pathinfo($_FILES['texto']['name'], PATHINFO_EXTENSION);
   $temporario = $_FILES['texto']['tmp_name'];
-  $palavra = file_get_contents($temporario);
+  $textoClaro = file_get_contents($temporario);
+  str_replace('\r\n', "", $textoClaro);
 
+  $formatosPermitidos = "txt";
+  $extensao = pathinfo($_FILES['inputChavePublica']['name'], PATHINFO_EXTENSION);
+  $temporario = $_FILES['inputChavePublica']['tmp_name'];
+  $chavePublica = file_get_contents($temporario);
+  // $chavePublica = preg_replace( "/\r|\n/", "", $chavePublica);
 
-  $arrayChaves = [$palavra];
+  $cipher = "AES-128-CBC";
 
-  echo json_encode($arrayChaves);
+  $iv = random_bytes(8);
+  $iv = bin2hex($iv);
+
+  $key = random_bytes(8);
+  $key = bin2hex($key);
+
+  $textoCifrado = openssl_encrypt($textoClaro, $cipher, $key, $options=0, $iv);
+
+  $arrayEncrypt = [$textoCifrado, $key, $iv];
+
+  echo json_encode($arrayEncrypt);
+
+}else{
+
+  $textoClaroOriginal = openssl_decrypt($textoCifrado, $cipher, $key, $options=0, $iv);
+
 }
 ?>
 

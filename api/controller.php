98be186ba04735785262084d7e9fb4557adfb49a
Renan Metzen <renanmetzen@caiweb.com.br>
2022-11-19 18:28:32 -0300

    enc simétrica ok

--- api/controller.php
+++ api/controller.php
@@ -10,8 +10,9 @@ if(isset($_POST['chaves'])){
   echo json_encode($arrayChaves);
 }elseif(isset($_FILES['texto'])){
   $arrayEncrypt = array();
-
+  define('CRYPT_RSA_PKCS15_COMPAT', true);
   ini_set( 'default_charset', 'utf-8');
+
   $formatosPermitidos = "txt";
   $extensao = pathinfo($_FILES['texto']['name'], PATHINFO_EXTENSION);
   $temporario = $_FILES['texto']['tmp_name'];
@@ -22,26 +23,54 @@ if(isset($_POST['chaves'])){
   $extensao = pathinfo($_FILES['inputChavePublica']['name'], PATHINFO_EXTENSION);
   $temporario = $_FILES['inputChavePublica']['tmp_name'];
   $chavePublica = file_get_contents($temporario);
-  // $chavePublica = preg_replace( "/\r|\n/", "", $chavePublica);
 
   $cipher = "AES-128-CBC";
-
   $iv = random_bytes(8);
   $iv = bin2hex($iv);
-
   $key = random_bytes(8);
   $key = bin2hex($key);
 
   $textoCifrado = openssl_encrypt($textoClaro, $cipher, $key, $options=0, $iv);
 
-  $arrayEncrypt = [$textoCifrado, $key, $iv];
+  // $rsa = new Crypt_RSA();
+  // $rsa->loadKey($chavePublica); // public key
+
+  // $rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+  // $ciphertext = $rsa->encrypt($textoCifrado);
+
+  $arrayEncrypt = [$textoCifrado, $key2, $iv2];
 
   echo json_encode($arrayEncrypt);
 
 }else{
+  $arrayEncrypt = array();
+  define('CRYPT_RSA_PKCS15_COMPAT', true);
+  ini_set( 'default_charset', 'utf-8');
+
+  $formatosPermitidos = "txt";
+  $extensao = pathinfo($_FILES['textoCifrado']['name'], PATHINFO_EXTENSION);
+  $temporario = $_FILES['textoCifrado']['tmp_name'];
+  $textoCifrado = file_get_contents($temporario);
+
+  $formatosPermitidos = "txt";
+  $extensao = pathinfo($_FILES['key']['name'], PATHINFO_EXTENSION);
+  $temporario = $_FILES['key']['tmp_name'];
+  $key = file_get_contents($temporario);
+
+  $formatosPermitidos = "txt";
+  $extensao = pathinfo($_FILES['iv']['name'], PATHINFO_EXTENSION);
+  $temporario = $_FILES['iv']['tmp_name'];
+  $iv = file_get_contents($temporario);
+
+  $formatosPermitidos = "txt";
+  $extensao = pathinfo($_FILES['inputChavePrivada']['name'], PATHINFO_EXTENSION);
+  $temporario = $_FILES['inputChavePrivada']['tmp_name'];
+  $chavePrivada = file_get_contents($temporario);
+  $cipher = "AES-128-CBC";
 
   $textoClaroOriginal = openssl_decrypt($textoCifrado, $cipher, $key, $options=0, $iv);
 
+  echo json_encode($textoClaroOriginal);
 }
 ?>
 
